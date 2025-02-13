3f855be186567c17647178e75fa8eca1fe4c58da
포스코DX 청년 IT 전문가 9기 <bitacademy.poscodx@gmail.com>
2025-02-13 16:10:43 +0900

    Add files via upload

--- 13.Integration/kanbanboard/frontend/src/KanbanBoard.js
+++ 13.Integration/kanbanboard/frontend/src/KanbanBoard.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CardList from './CardList.js';
+import './assets/scss/KanbanBoard.scss';
 
-function KanbanBoard() {
+import data from './assets/json/data.js';
+
+const KanbanBoard = () => {
+    const [cards, setCards] = useState(data);
+
     return (
-        <>
-            <h4>Kanbanboard  과제</h4>
-        </>
+            <div className={'Kanban_Board'}>
+                <CardList 
+                    key={'To Do'}
+                    title={'To Do'}
+                    cards={cards.filter(card => card.status === 'ToDo')} />
+                <CardList
+                    key={'Doing'}
+                    title={'Doing'}
+                    cards={cards.filter(card => card.status === 'Doing')} />
+                <CardList
+                    key={'Done'}
+                    title={'Done'}
+                    cards={cards.filter(card => card.status === 'Done')} />
+                </div>
     );
-}
+};
 
 export default KanbanBoard;
