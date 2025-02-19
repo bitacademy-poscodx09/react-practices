6e2baa5f134826d9ce0a90219a2c805107ea026a
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2025-02-19 11:46:14 +0900

    ajax #finish

--- 13.Integration/ajax-practices/backend/src/main/java/ajax/controller/api/ItemController.java
+++ 13.Integration/ajax-practices/backend/src/main/java/ajax/controller/api/ItemController.java
@@ -1,19 +1,25 @@
 package ajax.controller.api;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Predicate;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import ajax.domain.Item;
 import ajax.dto.JsonResult;
@@ -28,10 +34,42 @@ public class ItemController {
 	public ItemController(@Qualifier("items") List<Item> items) {
 		this.items = items;
 	}
-	
-	@PostMapping
+
+	@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	public ResponseEntity<JsonResult<Item>> create(Item item, MultipartFile file){
+		log.info("Request[POST /item, Content-Type: multipart/form-data][{}, {}]", item,file.getOriginalFilename());
+		try {
+			final String saveFilename = UUID
+				.randomUUID()
+				.toString()
+				.concat(".")
+				.concat(file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.') + 1));
+			
+			Files.write(Files
+				.createDirectories(Paths.get("/ajax-practices-uploads/images"))
+				.resolve(saveFilename), file.getBytes());
+			
+			Long maxId = Optional
+				.ofNullable(items.isEmpty() ? null : items.getFirst())
+				.map(t -> t.getId())
+				.orElse(0L);
+				
+			item.setId(maxId + 1);
+			item.setImage("/assets/images/" + saveFilename);
+			items.addFirst(item);			
+			
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(JsonResult.success(item));
+			
+		} catch(Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<JsonResult<Item>> create(@RequestBody Item item){
-		log.info("Request[POST /api, Content-Type: application/json][{}]", item);
+		log.info("Request[POST /item, Content-Type: application/json][{}]", item);
 		
 		Long maxId = Optional
 			.ofNullable(items.isEmpty() ? null : items.getFirst())
@@ -48,7 +86,7 @@ public class ItemController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<JsonResult<Item>> read(@PathVariable Long id) {
-		log.info("Request[GET /api/{}]", id);
+		log.info("Request[GET /item/{}]", id);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
@@ -61,16 +99,32 @@ public class ItemController {
 	
 	@GetMapping
 	public ResponseEntity<JsonResult<List<Item>>> read() {
-		log.info("Request[GET /api]");
+		log.info("Request[GET /item]");
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(JsonResult.success(items));
 	}
 	
+	@PutMapping("/{id}")
+	public ResponseEntity<JsonResult<Item>> update(@PathVariable Long id, Item item) {
+		log.info("Request[PUT /item/{}, Content-Type: application/x-www-form-urlencoded][{}]", id, item);
+		
+		int index = items.indexOf(new Item(id));
+		Optional<Item> optionalItem = Optional.ofNullable(index == -1 ? null : items.get(index));
+		optionalItem.ifPresent((Item t) -> {
+			t.setName(item.getName());
+			t.setType(item.getType());
+		});
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(JsonResult.success(optionalItem.orElse(null)));
+	}
+	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<JsonResult<Long>> delete(@PathVariable Long id) {
-		log.info("Request[DELETE /api/{}]", id);
+		log.info("Request[DELETE /item/{}]", id);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
