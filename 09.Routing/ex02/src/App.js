5e1f6122fe139b79543122c54e4aa2967b101089
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2025-02-20 15:26:01 +0900

    router #finish

---
+++ 09.Routing/ex02/src/App.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react';
+import Main from "./component/Main";
+import Gallery from "./component/Gallery";
+import Guestbook from "./component/Guestbook";
+import Error404 from '../../ex01/src/component/Error404';
+
+
+const Router = () => {
+    const [route, setRoute] = useState({page: window.location.href.substring(window.location.href.lastIndexOf('/'))});
+
+    useEffect(() => {
+        const pushstateHandler = (e) => {
+            setRoute(e.detail ? e.detail : {page: '/'});
+        };
+    
+        const popstateHandler = (e) => {
+            setRoute(e.state ? e.state : {page: '/'});
+        }
+
+        window.addEventListener('pushstate', pushstateHandler);
+        window.addEventListener('popstate', popstateHandler);
+
+        return () => {
+            window.removeEventListener('pushstate', pushstateHandler);
+            window.removeEventListener('popstate', popstateHandler);
+        };
+    }, []);
+
+    let component = null;
+
+    switch(route.page) {
+        case '' :
+        case '/' :
+            component = <Main />;
+            break;
+        case '/guestbook' :
+            component = <Guestbook />;
+            break;
+        case '/gallery':
+            component = <Gallery />;
+            break;
+        default :
+            component = <Error404 />;                    
+    }
+
+    return component;
+};
+
+
+export default function App() {
+
+    const clickHandler = (e) => {
+        e.preventDefault();
+
+        const url = e.target.href.substring(e.target.href.lastIndexOf("/"));
+        window.history.pushState({page: url}, e.target.txt, url);
+        
+        window.dispatchEvent(new CustomEvent('pushstate', {detail: {page: url}}));
+    }
+
+    return (
+        <div>
+            <ul>
+                <li><a href={'/'} onClick={clickHandler}>[Main]</a></li>
+                <li><a href={'/gallery'} onClick={clickHandler}>[Gallery]</a></li>
+                <li><a href={'/guestbook'} onClick={clickHandler}>[Guestbook]</a></li>
+            </ul>
+            <Router />
+        </div>
+    )
+}
---
+++ 09.Routing/ex04/src/component/Guestbook.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import {NavLink} from 'react-router-dom';
+import {useNavigate} from 'react-router';
+
+export default function Guestbook() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            // window.location.href = '/gallery';
+            navigate('/error/500');
+        }, 3000);
+    }, []);
+
+    return (
+        <>
+            <ul>
+                <li><NavLink className={'dqwdqw'} to={'/'}>[Main]</NavLink></li>
+                <li><NavLink to={'/guestbook'}>[Guestbook]</NavLink></li>
+                <li><NavLink to={'/gallery'}>[Gallery]</NavLink></li>
+            </ul>
+            <div>
+                <h1>Guestbook</h1>
+            </div>
+        </>
+    );
+}
